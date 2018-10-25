4c60b3866e34f3ffbcfd8f72aeacc2a2a6cd4b17
TAKII KENSUKE <takii.kensuke.26z@st.kyoto-u.ac.jp>
2018-10-25 15:36:49 +0900

    taskA4 finished?

--- task4_andmore.py
+++ task4_andmore.py
@@ -15,12 +15,21 @@ CLASS = 10
 ACTFUN = 1
 
 # Optimization techniques
-# 0: SGD, 1: Momentum SGD, 2: AdaGrad
-OPTTECH = 2
+# 0: SGD, 1: Momentum SGD, 2: AdaGrad, 3: RMSProp, 4: AdaDelta, 5: Adam
+OPTTECH = 5
 
 ALPHA = 0.9         # for Momentum SGD
 ETA_ADAG = 0.001    # for AdaGrad
 ETA = 0.01          # Learning rate
+
+RHO = 0.9           # RMSProp
+RHO_DELTA = 0.95    # AdaDelta
+EPSILON = 0.00000001    # RMSProp
+EPSILON_D = 0.000001    # AdaDelta
+ALPHA_ADAM = 0.001  # Adam
+BETA_1 = 0.9
+BETA_2 = 0.999
+EPSILON_A = 0.00000001  # Adam
 
 
 # Sigmoid function (as activate function)
@@ -42,6 +51,31 @@ def adagrad(h, W, Wgrad):
     h = h + Wgrad * Wgrad
     W = W - ETA_ADAG * (1. / np.sqrt(h)) * Wgrad
     return h, W
+
+# RMSProp
+def rmsprop(h, W, Wgrad):
+    h = RHO * h + (1 - RHO) * Wgrad * Wgrad
+    W = W - ETA_ADAG / (np.sqrt(h) + EPSILON) * Wgrad
+    return h, W
+
+# AdaDelta
+def adadelta(h, s, W, Wgrad):
+    h = RHO_DELTA * h + (1 - RHO_DELTA) * Wgrad * Wgrad
+    deltaW = -1. * np.sqrt((s + EPSILON_D) / (h + EPSILON_D)) * Wgrad
+    s = RHO_DELTA * s + (1 - RHO_DELTA) * deltaW * deltaW
+    W = W + deltaW
+    return h, s, W
+
+# Adam
+def adam(t, m, v, W, Wgrad):
+    t = t + 1
+    m = BETA_1 * m + (1 - BETA_1) * Wgrad
+    v = BETA_2 * v + (1 - BETA_2) * Wgrad * Wgrad
+    mhat = m / (1 - BETA_1 ** t)
+    vhat = v / (1 - BETA_2 ** t)
+    W = W - ALPHA_ADAM * mhat / (np.sqrt(vhat) + EPSILON_A)
+    return t, m, v, W
+
 
 # Cross entropy error
 def cEntropy(y, y2):
@@ -69,8 +103,8 @@ def layer(x, W, b, actfun):
         return softmax(t)
 
 
-BATCH = 500
-EPOCH = 500
+BATCH = 512
+EPOCH = 784
 l_or_t = input("Learning or training? (Training : 0, Testing : 1) : ")
 l_or_t = int(l_or_t)
 if l_or_t == 0:
@@ -81,7 +115,7 @@ if l_or_t == 0:
     Y = np.array(Y)
 
     # Choose batches at random
-    arr_idx = np.random.choice(PIC_LEARN, BATCH)
+    arr_idx = np.random.choice(PIC_LEARN, BATCH, replace=False)
 
     Xmat = X[arr_idx[0]].ravel()
     Xmat = np.reshape(Xmat, (Xmat.shape[0], 1))
@@ -112,6 +146,30 @@ if l_or_t == 0:
         h_W2 = np.full((CLASS, M), 0.00000001)
         h_b1 = np.full((M, 1), 0.00000001)
         h_b2 = np.full((CLASS, 1), 0.00000001)
+    if OPTTECH == 3:
+        h_W1 = np.zeros((M, SIZEX * SIZEY))
+        h_W2 = np.zeros((CLASS, M))
+        h_b1 = np.zeros((M, 1))
+        h_b2 = np.zeros((CLASS, 1))
+    if OPTTECH == 4:
+        h_W1 = np.zeros((M, SIZEX * SIZEY))
+        h_W2 = np.zeros((CLASS, M))
+        h_b1 = np.zeros((M, 1))
+        h_b2 = np.zeros((CLASS, 1))
+        s_W1 = np.zeros((M, SIZEX * SIZEY))
+        s_W2 = np.zeros((CLASS, M))
+        s_b1 = np.zeros((M, 1))
+        s_b2 = np.zeros((CLASS, 1))
+    if OPTTECH == 5:
+        t_W1, t_W2, t_b1, t_b2 = 0, 0, 0, 0
+        m_W1 = np.zeros((M, SIZEX * SIZEY))
+        m_W2 = np.zeros((CLASS, M))
+        m_b1 = np.zeros((M, 1))
+        m_b2 = np.zeros((CLASS, 1))
+        v_W1 = np.zeros((M, SIZEX * SIZEY))
+        v_W2 = np.zeros((CLASS, M))
+        v_b1 = np.zeros((M, 1))
+        v_b2 = np.zeros((CLASS, 1))
 
     for ep in range(EPOCH):
         entropy_ave = 0
@@ -179,6 +237,22 @@ if l_or_t == 0:
             h_b2, b2 = adagrad(h_b2, b2, En_over_b2)
             h_W1, W1 = adagrad(h_W1, W1, En_over_W1)
             h_b1, b1 = adagrad(h_b1, b1, En_over_b1)
+        elif OPTTECH == 3:
+            h_W2, W2 = rmsprop(h_W2, W2, En_over_W2)
+            h_b2, b2 = rmsprop(h_b2, b2, En_over_b2)
+            h_W1, W1 = rmsprop(h_W1, W1, En_over_W1)
+            h_b1, b1 = rmsprop(h_b1, b1, En_over_b1)
+        elif OPTTECH == 4:
+            h_W2, s_W2, W2 = adadelta(h_W2, s_W2, W2, En_over_W2)
+            h_b2, s_b2, b2 = adadelta(h_b2, s_b2, b2, En_over_b2)
+            h_W1, s_W1, W1 = adadelta(h_W1, s_W1, W1, En_over_W1)
+            h_b1, s_b1, b1 = adadelta(h_b1, s_b1, b1, En_over_b1)
+        elif OPTTECH == 5:
+            t_W2, m_W2, v_W2, W2 = adam(t_W2, m_W2, v_W2, W2, En_over_W2)
+            t_b2, m_b2, v_b2, b2 = adam(t_b2, m_b2, v_b2, b2, En_over_b2)
+            t_W1, m_W1, v_W1, W1 = adam(t_W1, m_W1, v_W1, W1, En_over_W1)
+            t_b1, m_b1, v_b1, b1 = adam(t_b1, m_b1, v_b1, b1, En_over_b1)
+
 
         np.savez("test.npz", W1, b1, W2, b2)
 
